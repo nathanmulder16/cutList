e12a373aff555d9169549957839c0ca86c707eb1
Nathan Mulder <nathanmulder16@gmail.com>
2025-06-15 11:35:06 -0400

    feat: setup creation of cutlist

--- main.py
+++ main.py
@@ -4,7 +4,12 @@ import matplotlib.pyplot as plt
 
 def main():
     df = pd.read_csv("cuts.csv")
-    print(df)
+    print(df["length"][0])
+    
+    # create list of cuts
+
+def cutList(df) -> list:
+    pass
 
 
 if __name__ == "__main__":
