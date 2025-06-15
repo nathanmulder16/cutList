12b75cd66273bb1aa5244a07e0d3d3ac34658c2c
Nathan Mulder <nathanmulder16@gmail.com>
2025-06-15 11:47:01 -0400

    feat: complete createCutList func

--- main.py
+++ main.py
@@ -3,13 +3,21 @@ import matplotlib.pyplot as plt
 
 
 def main():
-    df = pd.read_csv("cuts.csv")
-    print(df["length"][0])
-    
+    df = pd.read_csv("cuts.csv")    
     # create list of cuts
+    cut_list = createCutList(df)
+    print(cut_list)
 
-def cutList(df) -> list:
-    pass
+def createCutList(df) -> list:
+    quantity_list = list(df["quantity"])
+    length_list = list(df["length"])
+    cut_list = []
+    for i in range(len(quantity_list)):
+        quantity = quantity_list[i]
+        length = length_list[i]
+        for _ in range(quantity):
+            cut_list.append(length)
+    return cut_list
 
 
 if __name__ == "__main__":
