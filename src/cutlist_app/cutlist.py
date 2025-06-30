4683f275f4b978792389101bf15c89d3c134e0a0
Nathan Mulder <nathanmulder16@gmail.com>
2025-06-30 18:24:54 -0400

    feat: add create_stacked_chart()

--- src/cutlist_app/cutlist.py
+++ src/cutlist_app/cutlist.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import streamlit as st
+import plotly.graph_objects as go
+import plotly.express as px
 import os
 
 st.set_page_config(
@@ -16,6 +18,7 @@ def updatePurchasedBoardLength():
     if st.session_state.purchased_length >= st.session_state.min_purchased_length:
         if st.session_state.purchased_length != st.session_state.prev_purchased_length:
             st.session_state.updatePurchasedBoardResult = True
+
 
 # [ ]: remove this as it is not called
 def hideUploader():
@@ -101,6 +104,92 @@ def createCutList(df, MAX_BOARD_LENGTH) -> pd.DataFrame:
     return cut_list
 
 
+def create_stacked_chart(df):
+    df["color"] = ""
+    kerf_color = "#FB0D0D"
+    color_options = [
+        "#2E91E5",
+        "#E15F99",
+        "#1CA71C",
+        "#DA16FF",
+        "#222A2A",
+        "#B68100",
+        "#750D86",
+        "#EB663B",
+        "#511CFB",
+        "#00A08B",
+        "#FB00D1",
+        "#FC0080",
+        "#B2828D",
+        "#6C7C32",
+        "#778AAE",
+        "#862A16",
+        "#A777F1",
+        "#620042",
+        "#1616A7",
+        "#DA60CA",
+        "#6C4516",
+        "#0D2A63",
+        "#AF0038",
+    ]
+
+    unique_cuts = df["cut_id"].unique()
+    unique_color_counter = 0
+    for unique_cut in unique_cuts:
+        if unique_cut == "kerf":
+            df.loc[df["cut_id"] == unique_cut, "color"] = kerf_color
+        else:
+            unique_color = color_options[unique_color_counter]
+            df.loc[df["cut_id"] == unique_cut, "color"] = unique_color
+            unique_color_counter = (unique_color_counter + 1) % len(color_options)
+
+    # set for remembering types
+    remember = set()
+    fig = go.Figure()
+    for length, board, cut, color in zip(df.length, df.board_id, df.cut_id, df.color):
+        fig.add_bar(
+            x=[length],
+            y=[board],
+            marker_color=color,
+            orientation="h",
+            showlegend=cut not in remember,  # decide if trace is shown in legend
+            name=cut,
+            text=cut,
+            textposition="auto",
+        )
+        remember.add(cut)  # add current type to set
+
+    fig.update_layout(
+        xaxis={"categoryorder": "array", "categoryarray": []},
+        # title={
+        #     "text": f"<b>{project_title}</b>",
+        #     "y": 0.97,
+        #     "x": 0.5,
+        #     "xanchor": "center",
+        #     "yanchor": "top",
+        # },
+        xaxis_title="Length (in)",
+        yaxis_title="Board #",
+        legend=dict(
+            title="Cut Length",
+            orientation="h",
+            yanchor="top",
+            y=1.2,
+            xanchor="left",
+            x=0,
+        ),
+        showlegend=True,
+        # width=1200,
+        # height=250,
+    )
+    fig.update_traces(marker=dict(line=dict(width=1, color="black")))
+
+    # switch to stacked bar
+    fig.update_layout(barmode="stack")
+    # fig.show()
+    return fig
+
+
 # for testing pytest setup
 def addSomeNumbers(lst):
     ans = 0
@@ -108,6 +197,31 @@ def addSomeNumbers(lst):
         ans += val
     return ans
 
+
+###################################################
+# Debugging
+test_df = pd.DataFrame(
+    {
+        "length": [60, 0.125, 30, 0.125, 60, 0.125, 30, 0.125, 60, 0.125],
+        "board_id": [1, 1, 1, 1, 2, 2, 2, 2, 3, 3],
+        "cut_id": [
+            "60",
+            "0.125",
+            "30",
+            "0.125",
+            "60",
+            "0.125",
+            "30",
+            "0.125",
+            "60",
+            "0.125",
+        ],
+    }
+)
+# Plotly Graph Object
+fig2 = create_stacked_chart(test_df)
+st.plotly_chart(fig2, on_select="ignore")
+###################################################
 
 # Logo and Title
 _, col2, col3 = st.columns([1, 1, 3])
@@ -216,7 +330,9 @@ with st.sidebar:
             )
         with col2:
             csv = convert_df(st.session_state.pieces)
-            st.download_button("Export BOM", csv, "bom.csv", "text/csv", key="download-csv")
+            st.download_button(
+                "Export BOM", csv, "bom.csv", "text/csv", key="download-csv"
+            )
 
 
 # Charts
@@ -229,34 +345,7 @@ if "pieces" in st.session_state:
             cut_list_per_wxh = createCutList(
                 relevant_pieces_df, st.session_state.purchased_length
             )
-            
-            # Debugging
-            test_df = pd.DataFrame(
-                {
-                    "length": [60, 0.125, 30, 0.125, 60, 0.125, 30, 0.125, 60, 0.125],
-                    "board_id": [1, 1, 1, 1, 2, 2, 2, 2, 3, 3],
-                    "cut_id": ["60", "0.125", "30", "0.125", "60", "0.125", "30", "0.125", "60", "0.125"]
-                }
-            )
-            #[ ]: try altair?
-            #[ ]: just check length with kerf but don't plot it
-            st.bar_chart(
-                test_df,
-                x="board_id",
-                y="length",
-                color="cut_id",
-                horizontal=True,
-                x_label="Length (in)",
-                y_label="Board",
-                use_container_width=False,
-                width=1000,
-                height=400
-                )
-            
 
-            st.write(cut_list_per_wxh)
-            #
-            
             st.subheader(each_wxh)
             st.bar_chart(
                 cut_list_per_wxh,
