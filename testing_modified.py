4c6956611af1970068a9db55a24f0c189d38b7a0
Nathan Mulder <nathanmulder16@gmail.com>
2025-06-30 18:01:18 -0400

    feat: create graph object

--- testing_modified.py
+++ testing_modified.py
@@ -1,34 +1,59 @@
 import pandas as pd
-import plotly.express as px
 import plotly.graph_objects as go
-import random
 
 
 def create_stacked_chart(x_data, y_data, z_data, project_title):
 
-    data = {"x": [i for i in range(len(x_data))], "duration": y_data, "type": x_data}
+    data = {"length": y_data, "board_id": x_data, "cut_id": z_data, "color": ""}
     df = pd.DataFrame(data)
 
+    kerf_color = "#FB0D0D"
 
-    df["cut_id"] = z_data
-    print(df["cut_id"].unique())
-    # [ ]: go through unique list and assign a color from color list
-    # [ ]: if out of colors, reset the list
-    # [ ]: create a df["color"] that correlates with cut_id
-    
-    df.drop(["x"], axis=1, inplace=True)
-    print(df)
+    color_options = [
+        "#2E91E5",
+        "#E15F99",
+        "#1CA71C",
+        "#DA16FF",
+        "#222A2A",
+        "#B68100",
+        "#750D86",
+        "#EB663B",
+        "#511CFB",
+        "#00A08B",
+        "#FB00D1",
+        "#FC0080",
+        "#B2828D",
+        "#6C7C32",
+        "#778AAE",
+        "#862A16",
+        "#A777F1",
+        "#620042",
+        "#1616A7",
+        "#DA60CA",
+        "#6C4516",
+        "#0D2A63",
+        "#AF0038",
+    ]
+
+    unique_cuts = df["cut_id"].unique()
+    unique_color_counter = 0
+    for unique_cut in unique_cuts:
+        if unique_cut == "kerf":
+            df.loc[df["cut_id"] == unique_cut, "color"] = kerf_color
+        else:
+            unique_color = color_options[unique_color_counter]
+            df.loc[df["cut_id"] == unique_cut, "color"] = unique_color
+            unique_color_counter = (unique_color_counter + 1) % len(color_options)
+
     # set for remembering types
     remember = set()
     fig = go.Figure()
-    for duration, text, cut in zip(df.duration, df.type, df.cut_id):
+    for length, text, cut, color in zip(df.length, df.board_id, df.cut_id, df.color):
         fig.add_bar(
-            x=[duration],
+            x=[length],
             y=[text],
-            # marker_color=color,
-            # marker_color=df["cut_id"],
+            marker_color=color,
             orientation="h",
-            # hovertext=cut,
             showlegend=cut not in remember,  # decide if trace is shown in legend
             name=cut,
             text=cut,
@@ -49,69 +74,26 @@ def create_stacked_chart(x_data, y_data, z_data, project_title):
         yaxis_title="Board #",
         legend=dict(
             title="Cut Length",
-            orientation="h",
+            orientation="v",
             yanchor="top",
             y=1.5,
             xanchor="left",
             x=0,
         ),
         showlegend=True,
-        width=1200,
-        height=250,
-        colorway=[
-            "#2E91E5",
-            "#E15F99",
-            "#1CA71C",
-            "#FB0D0D",
-            "#DA16FF",
-            "#222A2A",
-            "#B68100",
-            "#750D86",
-            "#EB663B",
-            "#511CFB",
-            "#00A08B",
-            "#FB00D1",
-            "#FC0080",
-            "#B2828D",
-            "#6C7C32",
-            "#778AAE",
-            "#862A16",
-            "#A777F1",
-            "#620042",
-            "#1616A7",
-            "#DA60CA",
-            "#6C4516",
-            "#0D2A63",
-            "#AF0038",
-        ],
+        # width=1200,
+        # height=250,
     )
-
-    fig.update_traces(marker=dict(line=dict(width=0.25, color="black")))
+    fig.update_traces(marker=dict(line=dict(width=1, color="black")))
 
     # switch to stacked bar
     fig.update_layout(barmode="stack")
     fig.show()
 
 
-print(px.colors.qualitative.Dark24)
-
-# using the event type from this dict
-color_map = {
-    "create": "#3288bd",
-    "update": "#fee08b",
-    "delete": "#4d4d4d",
-    "paste": "#fc8d59",
-    "copy": "#fc8d59",
-    "copy_paste": "#fc8d59",
-    "run_success": "#99d594",
-    "run_fail": "#d53e4f",
-    "enter_focus": "#e0e0e0",
-    "exit_focus": "#e0e0e0",
-}
-
 # x_data = [random.choice([*color_map.keys()]) for _ in range(40)]
-y_data = [60.0, 15.0, 15.0, 60.0, 60.0, 45.0, 45.0]
+y_data = [60.0, 15.0, 0.125, 15.0, 60.0, 60.0, 45.0, 45.0]
 # y_data = [random.random() for _ in range(40)]
-x_data = [1, 1, 1, 2, 3, 4, 4]
-z_data = ["60.0", "15.0", "15.0", "60.0", "60.0", "45.0", "45.0"]
-fig = create_stacked_chart(x_data, y_data, z_data, "Cut List")
+x_data = [1, 1, 1, 1, 2, 3, 4, 4]
+z_data = ["60.0", "15.0", "kerf", "15.0", "60.0", "60.0", "45.0", "45.0"]
+fig = create_stacked_chart(x_data, y_data, z_data, "2x4")
